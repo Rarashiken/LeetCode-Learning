069191afee4936211911ee9e140d81dbfe16b5fb
Rarashiken <91686758+Rarashiken@users.noreply.github.com>
2021-11-24 00:03:02 +0900

    Update leetcode-034.py

--- leetcode-034.py
+++ leetcode-034.py
@@ -30,6 +30,17 @@ class Solution(object):
         right = binary_search(nums, target, True)
         return [left,right]
 
+'''
+无脑用函数解法：
+class Solution:
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+        if not nums:
+            return [-1, -1]
+        
+        left,right = bisect.bisect_left(nums, target), bisect.bisect_right(nums, target)
+                
+        return [left, right - 1] if left < right else [-1, -1]
+'''
 
 
 
